86d8c618c36e4869d3238c2b7a2f6c148c7a8655
star_blue <15910787956@189.cn>
2017-12-08 16:50:21 +0800

    调试dictionary的api

--- app/lib/dictionary.js
+++ app/lib/dictionary.js
@@ -1,10 +1,17 @@
 const oxford = require('../dat/oxford.json');
 
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
 let dictionary = { undefined: [] };
 
-for (let i of 'abcdefghijklmnopqrstuvwxyz')
-    dictionary[i] = oxford.filter(value => value[0][0] === i);
+for(let i = alphabet.length; i >= 0; --i)
+    dictionary[alphabet[i]] = oxford.filter(value => value[0][0] == alphabet[i]);
 
-const startwith = prefix => dictionary[prefix[0]].filter(value => value[0].startsWith(prefix));
+const startwith = prefix => {
+    prefix = prefix.trim().toLowerCase();
+    if(prefix[0] < 'a' || prefix[0] > 'z')
+        return [];
+    return dictionary[prefix[0]].filter(value => value[0].startsWith(prefix));
+}
 
 module.exports = startwith;
